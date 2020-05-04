95e3ff6fe76d38e38d9a0289825072b678de3167
Iwona007 <iwonka.adamkiewicz@gmail.com>
2020-05-04 16:27:15 +0200

    change getAll

--- src/main/java/iwona/pl/modol12project/dao/DaoImpl.java
+++ src/main/java/iwona/pl/modol12project/dao/DaoImpl.java
@@ -1,9 +1,8 @@
 package iwona.pl.modol12project.dao;
 
 import iwona.pl.modol12project.model.Image;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -25,15 +24,15 @@ public class DaoImpl implements DaoRepo {
 
     @Override
     public List<Image> getAll() {
-        List<Image> newList = new ArrayList<>();
+//        List<Image> newList = new ArrayList<>();
         String sql = "SELECT url, content FROM " + NAME;
-//        return jdbcTemplate.query(sql, new BeanPropertyRowMapper(Image.class));
-        List<Map<String, Object>> maps = jdbcTemplate.queryForList(sql);
-        maps.stream().forEach(element -> newList.add(new Image(
-                Long.parseLong(String.valueOf(element.get("id"))),
-                String.valueOf( element.get("url")),
-                String.valueOf(element.get("content")))));
-        return newList;
+        return jdbcTemplate.query(sql, new BeanPropertyRowMapper(Image.class));
+//        List<Map<String, Object>> maps = jdbcTemplate.queryForList(sql);
+//        maps.stream().forEach(element -> newList.add(new Image(
+//                Long.parseLong(String.valueOf(element.get("id"))),
+//                String.valueOf( element.get("url")),
+//                String.valueOf(element.get("content")))));
+//        return newList;
     }
 
     @Override
